1944f2f96c649f8575086a9599ae87a6dba66625
kimmingyu4934-gif <kimmingyu4934@gmail.com>
2026-07-15 01:12:41 +0900

    Add files via upload

---
+++ mingyu.cpp
@@ -0,0 +1,112 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	for (int i = 1; i <= 100; i++)
+//	{
+//		cout << i << " ";
+//		if (i % 10 == 0)
+//			cout << endl;
+//	}
+// rerturn 0;
+//}
+
+//#include <iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	switch (n/10)
+//	{
+//		case 10:
+//			cout << "A+";
+//			break;
+//
+//		case 9:
+//			cout << "A";
+//			break;
+//
+//		case 8:
+//			cout << "B";
+//			break;
+//
+//		case 7:
+//			cout << "C";
+//			break;
+//
+//		case 6:
+//			cout << "D";
+//			break;
+//
+//		default:
+//			cout << "F";
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n,sum = 0;
+//	cin >> n;
+//	for (int i = 1; i <= n; i++)
+//		sum += i;
+//	cout << sum;
+//}
+
+#include <iostream>
+using namespace std;
+
+int sum2(int a)
+{
+	int n = a;
+	sum2()
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << sum2(n);
+}
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (n % i == 0)
+//			cout << i << " ";
+//	}
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int x;
+//	cin >> x;
+//
+//	for (int i = 1; i <= x; i++)
+//	{
+//		if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+//			cout << " ";
+//		else
+//			cout << i <<" ";
+//	}
+//	return 0;
+//}
