edcbb09001794965fbb48bfa857d84fc3eb13096
kimmingyu4934-gif <kimmingyu4934@gmail.com>
2026-07-22 14:35:53 +0900

    Add files via upload

--- mingyu.cpp
+++ mingyu.cpp
@@ -61,22 +61,6 @@
 //	cout << sum;
 //}
 
-#include <iostream>
-using namespace std;
-
-int sum2(int a)
-{
-	int n = a;
-	sum2()
-}
-
-int main()
-{
-	int n;
-	cin >> n;
-	cout << sum2(n);
-}
-
 //#include <iostream>
 //
 //using namespace std;
@@ -98,15 +82,157 @@ int main()
 //
 //int main()
 //{
-//	int x;
-//	cin >> x;
-//
-//	for (int i = 1; i <= x; i++)
+//	int p = 1;
+//	int input;
+//	for (int i = 1; ; i++)
 //	{
 //		if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
-//			cout << " ";
+//			i++;
+//		cout << "P" << p << endl << ">>";
+//		cin >> input;
+//		if (input != i)
+//			break;
+//
+//		if (p == 1)
+//			p++;
 //		else
-//			cout << i <<" ";
+//			p--;
 //	}
-//	return 0;
+//	if (p == 1)
+//		cout << "p2 win";
+//	else
+//		cout << "p1win";
+//}
+// 
+//--피보나치 수열(재귀호출)--
+//#include <iostream>
+//
+//using namespace std;
+//
+//int list1(int a, int *p, int k)
+//{
+//	if (a == k)
+//		return 0;
+//	p[k] = p[k - 1] + p[k - 2];
+//	return list1(a, p, k+1);
+//}
+//
+//int main()
+//{
+//	int lists[256];
+//	int n,a=2;
+//	cin >> n;
+//	lists[0] = 1;
+//	lists[1] = 1;
+//	list1(n, lists,a);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << lists[i] <<" ";
+//	}
+//}
+
+//버블정렬
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int lists[5] = { 5, 3, 1, 2, 1 };
+//    int store;
+//
+//    for (int i = 0; i < 4; i++)
+//    {
+//        for (int j = 0; j < 4 - i; j++)
+//        {
+//            if (lists[j] > lists[j + 1])
+//            {
+//                store = lists[j];
+//                lists[j] = lists[j + 1];
+//                lists[j + 1] = store;
+//            }
+//        }
+//    }
+//
+//    for (int i = 0; i < 5; i++)
+//    {
+//        cout << lists[i] << " ";
+//    }
+//
+//    return 0;
+//}
+//---선택정렬
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int lists[5] = { 5, 3, 1, 2, 1 };
+//	int min = lists[0], store;
+//	int	count = 0;
+//
+//	for (int i = 0; i < 5; i++)
+//	{
+//		if (min > lists[i])
+//		{
+//			min = lists[i];
+//		}
+//			
+//	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		store = lists[count];
+//		lists[count] = min;
+//		lists[i] = store;
+//		count++;
+//	}
+//	   for (int i = 0; i < 5; i++)
+//	   {
+//        cout << lists[i] << " ";
+//    }
+//}
+//--제곱수 구하기
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int x, y,store;
+//	cin >> x >> y;
+//	if (y < x)
+//	{
+//		store = x;
+//		x = y;
+//		y = store;
+//	}
+//
+//	for (int i = x; i <= y; i++)
+//	{
+//		if (i * i >= x && i * i <= y)
+//			cout << i * i << " ";
+//	}
+//}
+//--특정단어 바꾸기
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	string a;
+//	cin >> a;
+//	int b = a.length();
+//	string a_word, new_word;
+//
+//	while (true)
+//	{
+//		cout << "무엇을 무엇으로 바꾸고 싶나요?" << endl;
+//		cin >> a_word >> new_word;
+//		size_t pos = a.find(a_word);
+//		if (pos == string::npos)
+//			continue;
+//		else
+//			break;
+//	}
+//
+//	a.replace(a.find(a_word), a_word.length(), new_word);
+//	cout << a;
 //}
